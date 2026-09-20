b382217a190dd4f8a81c8eb94236663365b707ab
kikiBAEK <154071956+kikiBAEK@users.noreply.github.com>
2026-09-20 08:45:25 +0800

    Add files via upload

---
+++ level1/p02_is_prime/p02.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+bool isPrime(int n) 
+{
+    if (n <= 1) return false;          
+    if (n == 2) return true;           
+    if (n % 2 == 0) return false;      
+
+
+    for (int i = 3; i*i<= n; i += 2)
+    {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n;
+
+    cout << "请输入一个整数 n: ";
+    cin >> n;
+
+    if (isPrime(n)) {
+        cout << n << " 是素数。" << endl;
+    }
+    else {
+        cout << n << " 不是素数。" << endl;
+    }
+
+    return 0;
+}
